a37c036cb26f5b33c57be9e60732f3f35d2666cc
Mehboob Alam <mehboobalam.dev@gmail.com>
2025-06-25 18:02:08 +0500

    Concatenation in kotlin

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val name = "Kotlin"
+    var firstName = "Mehboob";
+    var lastName = "Alam";
+    var fullName = firstName + " " + lastName;
+    println(fullName);
+}
