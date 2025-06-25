88044a9cfd3e63db12f1eb698a4d107f27fdee0a
Mehboob Alam <mehboobalam.dev@gmail.com>
2025-06-25 18:24:44 +0500

    Assignment 1 Initialize num with 20. Add 10, Home Assigment 2 Assignment Question 1  Write a program to compare two numbers x = 15 and y = 25. Print whether x is less than y.

---
+++ src/Assignment2.kt
@@ -0,0 +1,11 @@
+fun  main(){
+//    Assignment Question 1
+//Write a program to compare two numbers x = 15 and y = 25. Print whether x is less than y.
+    val x = 15
+    val y = 25
+    if(x<y){
+        println("$x is less than $y")
+    }else{
+        println("$x is greater then $y")
+    }
+}
