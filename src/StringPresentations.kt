dd62d88a9110ebb6aee2c34d017fa8b4e8a99f72
Mehboob Alam <mehboobalam.dev@gmail.com>
2025-06-26 21:40:19 +0500

    Learning String

---
+++ src/StringPresentations.kt
@@ -0,0 +1,9 @@
+fun main(){
+//    learning string
+    var s ="Hello"
+    println(s)
+    var txt = "Hello World"
+    println(txt[0])             // first element (H)
+    println(txt[2])           // third element (l)
+
+}
