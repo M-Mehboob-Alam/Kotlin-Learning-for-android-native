01b8fc53451724cd4e3e548172e59300a83102c4
Mehboob Alam <mehboobalam.dev@gmail.com>
2025-06-26 22:10:52 +0500

    Array, declare access and modified

---
+++ src/ArrayPresentation.kt
@@ -0,0 +1,22 @@
+fun main(){
+//    val marks = arrayOf(10,9,3,4,5)
+//    val differentTypeArray = arrayOf(10,4,5,"Array string", 'c', 10.5f)
+//
+//    println(marks.get(0))
+
+//    val marks = arrayOf<Int>(10,4,5,8,9)
+//    println(marks.get(0))
+//    println(marks.get(1))
+//    // update the value of the first element of the array
+//    marks.set(0,100)
+//    println(marks.get(0))
+
+    val marks = arrayOf<Int>(10,4,5,8,9)
+    println(marks[0])
+    println(marks[1])
+    // setting new value for first element of array
+    marks[0] = 100
+    println(marks[0])
+
+
+}
