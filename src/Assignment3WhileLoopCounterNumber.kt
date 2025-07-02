d563af0aabafb6bb3d487466591833a8eac88a78
Mehboob Alam <mehboobalam.dev@gmail.com>
2025-07-02 22:14:37 +0500

    count number with while loop

--- src/Assignment3WhileLoopCounterNumber.kt
+++ src/Assignment3WhileLoopCounterNumber.kt
@@ -1,7 +1,19 @@
+import kotlin.math.abs
+
 fun main(){
 //    var number =4567
 //    Count Digits in a Number
-      var number = 4567
+      var number = 456786987
       var digit = 0
 //    while ()
+      number = abs(number)
+      if (number == 0){
+            println(1)
+      }
+      while (number > 0){
+            number /= 10
+            digit++
+      }
+      println("Total Number Are $digit")
+
 }
