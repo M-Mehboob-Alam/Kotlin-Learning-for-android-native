a9b6ba889623611f291ce10eee38d589756c2112
Mehboob Alam <mehboobalam.dev@gmail.com>
2025-07-03 22:16:29 +0500

    Do While Loop Print even numbers from 1 to 20

---
+++ src/DoWhileLoopAssignent2.kt
@@ -0,0 +1,11 @@
+fun main(){
+//    Print even numbers from 1 to 20
+    var number = 1
+     do {
+         if (number%2 ==0){
+             println(number)
+         }
+         number++
+
+     }while(number < 21)
+}
